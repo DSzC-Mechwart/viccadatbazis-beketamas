4f07f1dd3573b529903ac1a43b01e8e006f0d35c
beke.tamas <beke.tamas@BURG-1E06-19>
2024-10-09 09:14:17 +0200

    vic

---
+++ ViccAdatbazis/Controllers/NapiViccekController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViccAdatbazis.Models;
+
+namespace ViccAdatbazis.Controllers
+{
+    public class NapiViccekController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]")]
+
+        public Vicc Get()
+        {
+            Vicc newfunny = new();
+            newfunny.Tartalom = "A Pál utcai fiúk közül, Boka fog kirándulni a legtöbbször";
+            newfunny.Feltolto = "XD man";
+            newfunny.Tetszik = 500;
+            newfunny.NemTetszik = 1;
+            newfunny.Aktiv = true;
+            return newfunny;
+        }
+    }
+}
--- ViccAdatbazis/Program.cs
+++ ViccAdatbazis/Program.cs
@@ -4,27 +4,28 @@ using ViccAdatbazis.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ViccDbContext>(
     options => options.UseSqlite(connectionString: @"Data Source = .\Data\ViccAdatbazis.db"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
+app.MapControllers();
 app.MapRazorPages();
 
 app.Run();
